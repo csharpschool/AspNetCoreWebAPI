5666cff23544789cccece50d618f7b0b012f32f3
Jonas <N/A>
2017-08-23 10:47:19 +0200

    Adding the PublisherCreateDTO Class

---
+++ AspNetCoreWebAPI/Models/PublisherCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebAPI.Models
+{
+    public class PublisherCreateDTO
+    {
+        [Required(ErrorMessage = "You must enter a name.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int Established { get; set; }
+    }
+}
