aae546e7c616f5b9720154a1d9e4fb5d0d35278f
Jonas <N/A>
2017-08-23 14:43:52 +0200

    Adding the Publisher and Book Entity Classes

---
+++ AspNetCoreWebAPI/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCoreWebAPI.Entities
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [ForeignKey("PublisherId")]
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+    }
+}
---
+++ AspNetCoreWebAPI/Entities/Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCoreWebAPI.Entities
+{
+    public class Publisher
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int Established { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
