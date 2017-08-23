ff3eb4d96dfb4884388feca3f855b7b2d21fa18e
Jonas <N/A>
2017-08-23 10:26:37 +0200

    FIX: Added Route attribute to the PublishersController class

--- AspNetCoreWebAPI/Controllers/PublishersController.cs
+++ AspNetCoreWebAPI/Controllers/PublishersController.cs
@@ -3,12 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreWebAPI.Controllers
 {
+    [Route("api/publishers")]
     public class PublishersController : Controller
     {
         IBookstoreRepository _rep;
         public PublishersController(IBookstoreRepository rep)
         {
             _rep = rep;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_rep.GetPublishers());
         }
 
     }
