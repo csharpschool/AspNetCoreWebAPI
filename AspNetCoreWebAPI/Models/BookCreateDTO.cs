ed4d315bd9104f4043236b4e3780225bd857783d
Jonas <N/A>
2017-08-23 14:12:16 +0200

    Adding the BookCreateDTO Class

---
+++ AspNetCoreWebAPI/Models/BookCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebAPI.Models
+{
+    public class BookCreateDTO
+    {
+        [Required(ErrorMessage = "You must enter a title")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public int PublisherId { get; set; }
+    }
+}
