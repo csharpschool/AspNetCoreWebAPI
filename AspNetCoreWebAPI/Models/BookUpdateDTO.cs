7d42d72acd97273731efa730c65605d6c0252a9f
Jonas <N/A>
2017-08-23 14:20:52 +0200

    Adding the BookUpdateDTO model class

---
+++ AspNetCoreWebAPI/Models/BookUpdateDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreWebAPI.Models
+{
+    public class BookUpdateDTO
+    {
+        [Required(ErrorMessage = "You must enter a title")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public int PublisherId { get; set; }
+    }
+}
