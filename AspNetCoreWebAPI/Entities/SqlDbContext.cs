5797e42d5a6d2d8de4580dc5130c314861e1329f
Jonas <N/A>
2017-08-23 14:50:15 +0200

    Adding the SqlDbContext Class

---
+++ AspNetCoreWebAPI/Entities/SqlDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebAPI.Entities
+{
+    public class SqlDbContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
