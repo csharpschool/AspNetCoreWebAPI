02d5805be1ebb4ce859493dc6f87e6a1f258233f
Jonas <N/A>
2017-08-23 10:10:08 +0200

    Adding the Publishers Controller

---
+++ AspNetCoreWebAPI/Controllers/PublishersController.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebAPI.Controllers
+{
+    public class PublishersController : Controller
+    {
+        IBookstoreRepository _rep;
+        public PublishersController(IBookstoreRepository rep)
+        {
+            _rep = rep;
+        }
+
+    }
+}
