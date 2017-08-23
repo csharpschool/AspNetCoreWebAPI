ce8e49ef001c9a780f33d7284b4497de617369da
Jonas <N/A>
2017-08-23 13:58:09 +0200

    Adding the BooksController Class

---
+++ AspNetCoreWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebAPI.Controllers
+{
+    [Route("api/publishers")]
+    public class BooksController : Controller
+    {
+        IBookstoreRepository _rep;
+
+        public BooksController(IBookstoreRepository rep)
+        {
+            _rep = rep;
+        }
+    }
+}
