47876a5f220bac42a52b7d630f63bbe1fa80f36a
Jonas <N/A>
2017-08-23 09:56:12 +0200

    Creating the Main DTOs

---
+++ AspNetCoreWebAPI/Models/BookDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreWebAPI.Models
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublisherId { get; set; }
+    }
+}
---
+++ AspNetCoreWebAPI/Models/PublisherDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreWebAPI.Models
+{
+    public class PublisherDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Established { get; set; }
+
+        public int BookCount { get { return Books.Count; } }
+
+        public ICollection<BookDTO> Books { get; set; } = new List<BookDTO>();
+    }
+}
