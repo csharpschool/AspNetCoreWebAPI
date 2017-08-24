bcadb8d0bb3c34729bd04e743af05be3f80a4356
Jonas <N/A>
2017-08-24 16:16:37 +0200

    Fetching One Book (GET)

--- AspNetCoreWebAPI/Controllers/GenBooksController.cs
+++ AspNetCoreWebAPI/Controllers/GenBooksController.cs
@@ -28,5 +28,17 @@ namespace AspNetCoreWebAPI.Controllers
             return Ok(DTOs);
         }
 
+        [HttpGet("{publisherId}/books/{id}", Name = "GetGenericBook")]
+        public IActionResult Get(int publisherId, int id, bool includeRelatedEntities = false)
+        {
+            var item = _rep.Get<Book>(id, includeRelatedEntities);
+
+            if (item == null || !item.PublisherId.Equals(publisherId))
+                return NotFound();
+
+            var DTO = Mapper.Map<BookDTO>(item);
+            return Ok(DTO);
+        }
+
     }
 }
